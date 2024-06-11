1124870bd013bedf8f5ee87b335c72d5f9eb4579
Francesc Bernat Bieri Tauler <francescbernatf@gmail.com>
2024-06-11 12:33:00 +0200

    Lectura correcte de les dades.

--- P01_LecturaDades.py
+++ P01_LecturaDades.py
@@ -12,8 +12,22 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-files = "Dades/IB05*.csv"
+files = "Dades/IB0*.csv"
 
 arxius = glob.glob(files)
 
-df = pd.read_csv(arxius[0], sep=";", encoding='latin-1')
+df_Felanitx = pd.DataFrame()
+df_Albufera = pd.DataFrame()
+
+for arxiu in arxius:
+    df = pd.read_csv(arxiu, sep=";", decimal=",",
+                    encoding='UTF-16 LE', index_col="Fecha")
+    
+    if "Felanitx" in arxiu:
+        df_Felanitx = pd.concat( [df_Felanitx, df] )
+
+    elif "Sa Pobla" in arxiu:
+        df_Albufera = pd.concat( [df_Felanitx, df] )
+
+df_Felanitx = df_Felanitx.drop_duplicates(keep="first")
+df_Albufera = df_Albufera.drop_duplicates(keep="first")
